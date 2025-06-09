f94f90e7df4a5a2672098a0401f2850ed7c8c4c4
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-06-09 08:09:55 +0530

    Add created a server

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import app from "./app.js"
+import {config} from "dotenv"
+import { dbConnect } from "./db/index.js"
+
+config()
+
+try {
+    app.listen(process.env.PORT,()=>{
+        dbConnect()
+        console.log(`App Listening on port ${process.env.PORT}`)
+    })
+} catch (error) {
+    console.log(error)
+}
+
