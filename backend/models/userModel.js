46fdc8319f8dfff27e1adaed8bd2c9a7648dc20b
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-06-09 08:07:12 +0530

    Add user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose"
+
+const userSchema = mongoose.Schema({
+
+    username:{
+        type:String,
+        required:true,
+        minLength:3,
+        maxLength:50,
+        trim:true,
+        lowercase:true,
+        unique:true
+    },
+    firstname:{
+        type:String,
+        required:true,
+        minLength:5,
+        maxLength:50
+    },
+    lastname:{
+        type:String,
+        required:true,
+        minLength:5,
+        maxLength:50
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:6,
+        maxLength:15,
+    }
+}) 
+
+const User = mongoose.model("User",userSchema)
+
+export {User}
