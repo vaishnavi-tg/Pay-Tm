f264c9e1e02e864f7c47e552531f59ca26c77134
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-06-10 18:23:06 +0530

    Add update user features

---
+++ backend/middleware.js
@@ -0,0 +1,26 @@
+import JWT_SECRET from "./config.js"
+import jwt from "jsonwebtoken"
+
+
+const authMiddelware = (req,res,next)=>{
+    const authHeader = req.headers.authentication
+
+    if(!authHeader || !authHeader.startswith("Bearer")){
+        return res.status(403).json({})
+    }
+
+    const token = authHeader.split('')[1]
+
+    try{
+        const decoded = jwt.verify(token,JWT_SECRET)
+
+        req.userId  = decoded.userId
+
+        next()
+    }
+    catch(err){
+        return res.status(403).json({})
+    }
+}
+
+export {authMiddelware}
---
+++ backend/routes/update.js
@@ -0,0 +1,30 @@
+import { Router } from "express"
+import zod from "zod"
+import { authMiddelware } from "../middleware"
+import {User} from "./models/userModel.js"
+
+const updateRouter = Router()
+
+const updateBody = zod.object({
+    username: zod.string().optional(),
+    firstname: zod.string().optional(),
+    lastname: zod.string().optional()
+})
+
+updateRouter.post("/",authMiddelware,async(req,res)=>{
+    const {success} = updateBody.safeParse(req.body)
+
+    if(!success){
+        return res.status(411).json({
+            msg:"Error while updating the information"
+        })
+    }
+
+    await User.updateOne({ _id: req.userId }, req.body)
+
+    res.json({
+        message : "Updated successfully"
+    })
+})
+
+export { updateRouter }
