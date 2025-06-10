2bb881122e66ba45ecde25241f418f87d27aec35
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-06-10 11:37:24 +0530

    Created seperate user signin and signup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,12 @@
-import {Router} from "express"
+import { Router } from "express"
+import { signupRouter } from "./signup"
+import { signinRouter } from "./signin"
+
 
 const userRouter = Router()
 
+userRouter.use("signup", signupRouter)
+userRouter.use("signin", signinRouter)
 
 
-export{userRouter}
+export { userRouter }
