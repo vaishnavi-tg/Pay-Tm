1d95bf40a8b4878df01be042744cb41dd7c72d4a
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-06-11 07:44:08 +0530

    Add get users feature

---
+++ backend/routes/getUsers.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import {User} from "./models/userModel.js"
+
+const getUsersRouter = Router()
+
+getUsersRouter.get("/bulk", async (req, res) => {
+    try {
+        const filter = req.query.filter || "";
+
+        const users = await User.find({
+            $or: [
+                { firstname: { $regex: filter, $options: "i" } }, // case-insensitive
+                { lastname: { $regex: filter, $options: "i" } }
+            ]
+        });
+
+        res.json({
+            users: users.map(user => ({
+                username: user.username,
+                firstname: user.firstname,
+                lastname: user.lastname,
+                _id: user._id
+            }))
+        });
+    } catch (err) {
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+export {getUsersRouter}
