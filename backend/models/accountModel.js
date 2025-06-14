eb3455523353f07d8927afa3a4769619b090ba2d
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-06-14 13:41:56 +0530

    Added accountModel

---
+++ backend/models/accountModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+const accountSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User,
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+})
+
+const Account = mongoose.model("Account",accountSchema)
+
+export {Account}
