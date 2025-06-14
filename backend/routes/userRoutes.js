e237f4a27367fa95e5937c4b7bd27dbaf85b3262
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-06-14 12:07:20 +0530

    Added routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { signin, signup } from "../controllers/userController";
+
+const router = Router()
+
+router.post("/signup", signup)
+router.post("/signin", signin)
+
+export default router
+
+
