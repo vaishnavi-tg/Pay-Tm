090ddb04b15ca1f54c6980ff30d6a91431ea17f8
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-06-15 19:26:24 +0530

    COMPLETED BACKEND

--- backend/routes/accountRoutes.js
+++ backend/routes/accountRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { balance } from "../controllers/accountController.js";
+import { balance, transfer } from "../controllers/accountController.js";
 import { authMiddleware } from "../middleware.js";
 
 const router = Router()
 
 router.get("/balance", authMiddleware, balance)
 
-router.post("/transfer", transfer)
+router.post("/transfer", authMiddleware, transfer)
 
 export default router
