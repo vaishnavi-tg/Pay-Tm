b55dbaa2eb889284f7537b4af7ff0a8df72fb9ab
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-06-15 18:09:21 +0530

    Added accountcontrollers

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -1,16 +1,11 @@
 import { Account } from "../models/accountModel.js"
-import { Router } from "express"
-import { authMiddleware } from "../middleware.js"
 
-const balanceRouter = Router()
-
-balanceRouter.get("/balance", authMiddleware, async (req, res) => {
-    const account = await Account.findOne({
-        username: req.userId
-    })
+const balance =  async (req, res) => {
+  const account = await Account.findOne({ userId: req.userId });
+    console.log(account)
     res.json({
         balance: account.balance
     })
-})
+}
 
-export { balanceRouter }
+export { balance }
