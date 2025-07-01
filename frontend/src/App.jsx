140ceb9d7902fa2ff8cba026553ba0a05347a455
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-07-01 17:03:57 +0530

    update app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-function App(){
-    return(
-        <>
-        <BrowserRouter>
+function App() {
+  return (
+    <>
+      <BrowserRouter>
         <Routes>
-            <Route path="/signup" element={<Signup/>}/>
-            <Route path="/signin" element={<Signin/>}/>
-            <Route path="/dashboard" element={<Dashboard/>}/>
-            <Route path="/send" element={<SendMoney/>}/>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
         </Routes>
-        </BrowserRouter>
-        </>
-    )
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
