13f03d9bd594b0ad24985584bfc80f401cd98bbc
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-07-01 17:09:49 +0530

    update app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { SignUp } from "./pages/SignUp"
+import { SignIn } from "./pages/SignIn"
+import { Dashboard } from "./pages/Dashboard"
+import { SendMoney } from "./pages/SendMoney"
+
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<SendMoney />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
