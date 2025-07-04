4f4a519aeec990c26f00557f02b12239442351af
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-07-04 17:14:30 +0530

    Process

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { SignUp } from "./pages/SignUp"
-import { SignIn } from "./pages/SignIn"
-import { Dashboard } from "./pages/Dashboard"
-import { SendMoney } from "./pages/SendMoney"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { SignUp } from "./components/SignUp"
+import { SignIn } from "./components/SignIn"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/send" element={<SendMoney />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          
+
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
